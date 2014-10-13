1b7d47524894a0613b9e5e30feb124152a15407c
qianfeng <qianfeng@159.local>
2014-10-13 18:40:54 +0800

    Initial Commit

---
+++ 微博002/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  微博002
+//
+//  Created by qianfeng on 14-10-13.
+//  Copyright (c) 2014年 Glacier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
